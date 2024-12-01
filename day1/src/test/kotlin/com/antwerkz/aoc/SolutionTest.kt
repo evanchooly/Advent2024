bb3945d35149f1a07000edebf7195bd94ecf1392
evanchooly <jlee@antwerkz.com>
2024-12-01 18:03:24 -0500

    tighten up reading code add .sdkmanrc, formatting config

--- day1/src/test/kotlin/com/antwerkz/aoc/SolutionTest.kt
+++ day1/src/test/kotlin/com/antwerkz/aoc/SolutionTest.kt
@@ -9,28 +9,20 @@ class SolutionTest : TestBase() {
 
     override fun solvePart1(input: List<String>): Int {
         val (left, right) = readInput(input)
-
         return left.zip(right).sumOf { pair -> abs(pair.first - pair.second) }
     }
 
     override fun solvePart2(input: List<String>): Int {
         val (left, right) = readInput(input)
-
         return left.sumOf { id -> id * right.count { i -> i == id } }
     }
 
-    private fun readInput(input: List<String>): Pair<MutableList<Int>, MutableList<Int>> {
-        val left = mutableListOf<Int>()
-        val right = mutableListOf<Int>()
-        input
-            .map { line -> line.replace(Regex(" +"), " ").split(" ") }
-            .map { values -> listOf(values[0].toInt(), values[1].toInt()) }
-            .forEach { list ->
-                left += list[0]
-                right += list[1]
+    private fun readInput(input: List<String>): Pair<List<Int>, List<Int>> {
+        val pairs =
+            input.map { line ->
+                val ids = line.split("   ").map { it.toInt() }
+                ids[0] to ids[1]
             }
-        left.sort()
-        right.sort()
-        return Pair(left, right)
+        return pairs.map { it.first }.sorted() to pairs.map { it.second }.sorted()
     }
 }
